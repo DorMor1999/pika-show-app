de580d9274ad01975140503286030aa895c83bdb
DorMor1999 <dorm369258@gmail.com>
2024-11-01 18:42:41 +0200

    try

--- src/pages/series/SeriesPage.tsx
+++ src/pages/series/SeriesPage.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import Begin from '../../shared/components/MoviesAndSeries/Begin';
-import { options } from '../../shared/util/options';
+
 
 const SeriesPage: React.FC = () => {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZTZkZmJjM2NmNDYyNTk0ZDE4OWYyZGZmNTI4YmE4ZCIsIm5iZiI6MTczMDM4MTA2Mi4xNjc1MjM0LCJzdWIiOiI2NGQwZmVjMDg1MDkwZjAxMjViZDk4MDQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.pnG3jK5c7r-bdEimG0sth73XJEYJeYy6prNfoAEigok',
+    },
+  };
+
   const url: string =
     'https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc';
 
